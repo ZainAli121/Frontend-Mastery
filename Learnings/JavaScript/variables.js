c4587fbabcc231c7ae789ef0d49733ef3d8966ff
Zain <learnerstudent001@gmail.com>
2024-02-20 10:52:01 +0500

    JS variables

---
+++ Learnings/JavaScript/variables.js
@@ -0,0 +1,31 @@
+// alert("Hello world!");
+// console.log("Hello world!");
+
+let x =10.5;
+// alert(x);
+console.log(x);
+
+x= 40;
+console.log(x);
+
+const y = 50;
+console.log(y);
+
+// y= 60;
+// console.log(y);  we cannot reassign the value to a constant variable but we can change the value of let variable
+
+var z = 100;
+console.log(z);
+
+let a = "Zain";
+console.log(a);
+console.log(typeof a);
+
+let c = "Ali";
+console.log("My name is "+a +" "+ c);
+console.log(`My name is ${a} ${c}`);
+
+let b = true;
+console.log(b);
+
+console.log(x+z);
