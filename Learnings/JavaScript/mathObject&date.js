9e3eabe2b6a05afd784372b0eae8c62c2025a50e
Zain <learnerstudent001@gmail.com>
2024-03-13 15:17:54 +0500

    Math and Date Objects

---
+++ Learnings/JavaScript/mathObject&date.js
@@ -0,0 +1,16 @@
+let x = Math.round(4.6);
+console.log(x);
+
+let y = Math.floor(4.888888)
+console.log(y);
+
+let z = Math.pow(2, 3)
+console.log(z);
+
+let max = Math.max(3, 7, 8, 12, 43);
+console.log(max);
+
+const date = new Date("May 20 2024");
+const date1 = new Date();
+console.log(date);
+console.log(date1.getDay());
