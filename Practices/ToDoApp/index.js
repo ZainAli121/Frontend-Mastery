b25a04b6d2c790617ce293f51878f0226eae81ed
Zain <learnerstudent001@gmail.com>
2024-03-15 16:27:53 +0500

    JavaScript Project 4: Edit Task Functionality

--- Practices/ToDoApp/index.js
+++ Practices/ToDoApp/index.js
@@ -24,9 +24,19 @@ function get_all_tasks(){
         span.innerText = value.desc;
         innerDiv.append(span);
 
+        const edit_btn = document.createElement("button");
+        edit_btn.setAttribute("id", "edit");
+        edit_btn.innerText = "Edit";
+
+        edit_btn.addEventListener("click", ()=>{
+            editTask(index, p, span, edit_btn,btn,div);
+        });
+        div.append(edit_btn);
+
         const btn = document.createElement("button");
         btn.setAttribute("id", "del");
         btn.innerText = "Delete";
+
         btn.addEventListener("click", ()=>{
             remove_task();
             tasks_list.splice(index, 1);
@@ -34,6 +44,7 @@ function get_all_tasks(){
             get_all_tasks();
         })
         div.append(btn);
+
         container.append(div);
 
     });
@@ -47,6 +58,40 @@ function remove_task(){
     });
 };
 
+function editTask(index, p, span, edit_btn, del_btn, div) {
+    p.setAttribute("contenteditable", "true");
+    span.setAttribute("contenteditable", "true");
+    p.style.outline = "none";
+    p.style.border = "2px solid white";
+    span.style.outline = "none";
+    span.style.border = "2px solid white"
+    edit_btn.style.display = "none";
+    del_btn.style.display = "none";
+
+    const save_btn = document.createElement("button");
+    save_btn.setAttribute("id", "save_btn");
+    save_btn.innerText = "Save";
+
+    save_btn.addEventListener("click", () => {
+        saveTask(index, p.innerText, span.innerText);
+        save_btn.remove(); 
+        p.style.padding = "none";
+        p.style.outline = "none";
+        p.style.border = "none";
+        span.style.padding = "none";
+        span.style.outline = "none";
+        span.style.border = "none";
+        edit_btn.style.display = "block";
+        del_btn.style.display = "block";
+    });
+    div.append(save_btn);
+}
+
+function saveTask(index, newTitle, newDesc) {
+    tasks_list[index].title = newTitle;
+    tasks_list[index].desc = newDesc;
+    localStorage.setItem("tasks", JSON.stringify(tasks_list));
+}
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
