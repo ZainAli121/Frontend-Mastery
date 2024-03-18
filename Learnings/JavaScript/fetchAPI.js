1dd3d8bd78e5dc9d3c1fabbf1d06147fa0e699c9
Zain <learnerstudent001@gmail.com>
2024-03-18 15:18:24 +0500

    Async Await

--- Learnings/JavaScript/fetchAPI.js
+++ Learnings/JavaScript/fetchAPI.js
@@ -1,12 +1,37 @@
 const url = "https://dog.ceo/api/breeds/image/random";
 const img = document.querySelector("img");
 
-const fetchData = () => {
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => (img.src = data.message))
-    .catch(e=> console.log(e))
-    .finally(() => console.log("Done Execution!"))
-};
+// const fetchData = () => {
+//     fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => (img.src = data.message))
+//     .catch(e=> console.log(e))
+//     .finally(() => console.log("Done Execution!"))
+// };
+
+// fetchData();
+
+
+// ----------------------Async ----------------------->
+// const fetchData = async()=>{
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     img.src = data.message;
+// };
+
+// fetchData();
+
+const fetchData = async()=>{
+try{
+    const response = await fetch(url);
+    const data = await response.json();
+    img.src = data.message;
+}catch(error){
+    console.log(error.message);
+}
+finally{
+    console.log("Done!")
+}
+}
 
 fetchData();
